2a97290c018390bc7ece050cc0658ac6bf43e9f0
raghul <raghulkrishna50@gmail.com>
2023-09-13 18:54:24 +0530

    Test: pushing

--- components/blocks.tsx
+++ components/blocks.tsx
@@ -5,7 +5,7 @@ export default function Blocks() {
         <div className="py-12 md:py-20 border-t border-gray-800">
 
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-60">
             <h2 className="h2 mb-4">The majority our customers do not understand their workflows.</h2>
             <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
